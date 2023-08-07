a4c209d4edd1261706a93445471a7e2414cb19d9
Moonpig-GitHub <47976310+Moonpig-GitHub@users.noreply.github.com>
2023-08-07 16:05:23 +0100

    feat: added app.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/Page/Footer";
 import Header from "@/components/Page/Header";
-import "@/styles/globals.css";
+import "@/styles/_app.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
